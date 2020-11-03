e4b60b906a548403766d4f777af6b5ce4cfe93ba
Thiago <4077420+thiagovf@users.noreply.github.com>
2020-11-03 20:06:36 -0300

    Organizando, adicionando modal e dropdown

--- ProAgil-App/src/app/_helps/DateTimeFormat.pipe.spec.ts
+++ ProAgil-App/src/app/_helps/DateTimeFormat.pipe.spec.ts
@@ -2,10 +2,11 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { DateTimeFormatPipe } from './DateTimeFormat.pipe';
+import { Constants } from '../util/Constants';
 
 describe('Pipe: DateTimeFormate', () => {
   it('create an instance', () => {
-    let pipe = new DateTimeFormatPipe();
+    let pipe = new DateTimeFormatPipe(Constants.DATE_TIME_FMT);
     expect(pipe).toBeTruthy();
   });
 });
--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { DateTimeFormatPipe } from './_helps/DateTimeFormat.pipe';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
    declarations: [
@@ -18,9 +23,13 @@ import { FormsModule } from '@angular/forms';
    ],
    imports: [
       BrowserModule,
+      BsDropdownModule.forRoot(),
+      TooltipModule.forRoot(),
+      ModalModule.forRoot(),
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule
    ],
    bootstrap: [
       AppComponent
