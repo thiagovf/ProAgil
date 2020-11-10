4d9f70bba7f7640b4207fb085ba713d655807a6c
Thiago <4077420+thiagovf@users.noreply.github.com>
2020-11-10 06:33:23 -0300

    Adicionando annotations de validação no backend

--- ProAgil.Domain/Evento.cs
+++ ProAgil.Domain/Evento.cs
@@ -1,17 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProAgil.Domain
 {
     public class Evento
     {
         public int Id { get; set; }
+        [Required (ErrorMessage="Local deve ser preenchido.")]
+        [StringLength (100, MinimumLength=3, ErrorMessage="Local deve ter entre 3 e 100 caracteres.")]
         public string Local { get; set; }
         public DateTime DataEvento { get; set; }
+        [Required (ErrorMessage="O tema deve ser preenchido.")]
         public string Tema { get; set; }
+        [Range(1, 120000, ErrorMessage = "Qtd de pessoas é entre 2 e 120.000.")]
         public int QtdPessoas { get; set; }
         public string ImagemURL { get; set; }
+        [Phone]
         public string Telefone { get; set; }
+        [EmailAddress]
         public string Email { get; set; }
         public List<Lote> Lotes { get; set; }
         public List<RedeSocial> RedesSociais { get; set; }
