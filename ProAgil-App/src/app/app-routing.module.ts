a709cc12911f50829f4fbdb81d4b0d4d35bc0011
Thiago <4077420+thiagovf@users.noreply.github.com>
2020-11-13 06:22:10 -0300

    Adicionando rotas e alguns comonentes

--- ProAgil-App/src/app/app-routing.module.ts
+++ ProAgil-App/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatosComponent } from './contatos/contatos.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestranteComponent } from './palestrante/palestrante.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'eventos', component: EventosComponent },
+  { path: 'palestrantes', component: PalestranteComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'contatos', component: ContatosComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
