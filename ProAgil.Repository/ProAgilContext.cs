9e08e0817e36abd702ce059ad959bd6430eb343e
Thiago <4077420+thiagovf@users.noreply.github.com>
2020-11-23 05:47:51 -0300

    adicionando relacionamento

--- ProAgil.Repository/ProAgilContext.cs
+++ ProAgil.Repository/ProAgilContext.cs
@@ -1,10 +1,14 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProAgil.Domain;
+using ProAgil.Domain.identity;
 
 namespace ProAgil.Repository
 {
-    public class ProAgilContext : IdentityDbContext
+    public class ProAgilContext : IdentityDbContext<User, Role, int, IdentityUserClaim<int>, 
+                                                        UserRole, IdentityUserLogin<int>, IdentityRoleClaim<int>,
+                                                        IdentityUserToken<int>>
     {
         public ProAgilContext(DbContextOptions<ProAgilContext> options) : base (options) {}
         public DbSet<Evento> Eventos {get; set;}
@@ -15,6 +19,22 @@ namespace ProAgil.Repository
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserRole>(userRole => {
+                userRole.HasKey(ur => new {ur.UserId, ur.RoleId});
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+
             modelBuilder.Entity<PalestranteEvento>()
                 .HasKey(PE => new { PE.EventoId, PE.PalestranteId});
         }
